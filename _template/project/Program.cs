9c47a41da3d043cb623d4eb01a652ec3ccdc13a0
Brad Wilson <dotnetguy@gmail.com>
2020-12-10 22:11:04 -0800

    Update the template

--- _template/project/Program.cs
+++ _template/project/Program.cs
@@ -1,3 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-Console.WriteLine("Hello, world!");
+var data =
+	(from line in File.ReadAllLines("input.txt")
+	 where !string.IsNullOrWhiteSpace(line)
+	 select line).ToArray();
