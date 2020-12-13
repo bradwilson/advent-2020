3004fbc2176790f8a034a8f86d40f2806565adf8
Brad Wilson <dotnetguy@gmail.com>
2020-12-13 00:20:31 -0800

    Update template to support run timing

--- _template/project/Program.cs
+++ _template/project/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -7,3 +8,19 @@ var data =
 	(from line in File.ReadAllLines("input.txt")
 	 where !string.IsNullOrWhiteSpace(line)
 	 select line).ToArray();
+
+long GetPart1()
+{
+	return 0L;
+}
+
+long GetPart2()
+{
+	return 0L;
+}
+
+var stopwatch = Stopwatch.StartNew();
+Console.WriteLine($"Part 1: {GetPart1()}  ::  [{stopwatch.Elapsed}]");
+
+stopwatch = Stopwatch.StartNew();
+Console.WriteLine($"Part 2: {GetPart2()}  ::  [{stopwatch.Elapsed}]");
